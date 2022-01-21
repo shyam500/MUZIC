27d57b88e4bd9c74a958fd7b2e38fec7da3ba437
shyam500 <shyamppk2000@gmail.com>
2022-01-21 23:35:02 +0530

    play and pause working properly

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,23 @@ const song = new Audio("../audios/bensound-dubstep.mp3");
 
 all.playpause.addEventListener("click", () => playsongFunc(song));
 
-function playsongFunc(song){
-    song.play()
-    console.log(song.duration/60);
-    console.log(song.currentTime);
-};
+let playing = false;
+function playsongFunc(song) {
+  if (!playing) {
+    song.play();
+    all.playpause.src = "../images/pause.png";
+    timeFunc(all,song);
+    playing = true;
+  } else {
+    all.playpause.src = "../images/play.png";
+    playing = false;
+    song.pause();
+  } 
+}
 
- 
+function timeFunc({ start, end },song) {
+    const duration = song.duration;
+    end.textContent = ` ${Math.trunc(duration/60)}:${(Math.floor(duration%60))}min`;
+  console.log(start);
+  console.log(end);
+}
